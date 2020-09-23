796bdfab38e06845ba631284581c47cb73a8d71a
Pavan Patel <patelpav@bsd.k12.in.us>
2020-09-23 22:28:39 +0000

    loopingstar

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
 import turtle
-t = turtle.Turtle()
-t.speed(100)
-canvas = t.getscreen()
-canvas.bgcolor('gray')
-mycolors = ['red','orange','green','peachpuff','violet','white','blue','yellow']
-def flower(length,x,y):
-  t.penup()
-  t.goto(x,y)
-  t.pendown()
-  for c in mycolors:
-    t.pencolor(c)
-    for i in range(4):
-      t.rt(90)
-      t.forward(length) #end of square loop
-    t.penup()
-    t.rt(45)
-    t.pendown()  #color loop ends
-x = -120
-y = 120
-for i in range(5):
-  flower(20,x,y)
-  x = x + 60
-  y = y - 60
 
+turtle.showturtle()
+turtle.shape("turtle")
+turtle.pencolor('green')
+for x in range(13):
+        turtle.forward(200)
+        turtle.left(150)
