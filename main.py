5695da0ea2476ccf5d9672f288b547f1f3b16c21
Pavan Patel <patelpav@bsd.k12.in.us>
2020-09-28 23:27:05 +0000

    Example

--- main.py
+++ main.py
@@ -1,47 +1,14 @@
 import turtle
-length = 10
-angle = 90
-turtle.showturtle()
-turtle.shape("turtle")
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
-turtle.forward(length+length)
-turtle.right(angle)
-length = length + 10
+def shape(sides):
+  for i in range(sides):
+    turtle.fd(20)
+    turtle.rt(360/sides)
+turtle.color('red')
+shape(10)
+for n in range(9):
+  turtle.fd(50)
+  shape(4)
+turtle.goto(-100,100)
+for n in range(9):
+  turtle.fd(50)
+  turtle.rt(160)
