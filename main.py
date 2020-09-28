11269bcbb9a2c393b104b534282d9cabcf44f6c4
Pavan Patel <patelpav@bsd.k12.in.us>
2020-09-28 23:39:02 +0000

    Challenge 1

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ def shape(sides):
     turtle.fd(20)
     turtle.rt(360/sides)
 turtle.color('red')
-shape(10)
-for n in range(9):
+shape(3)
+for n in range(1):
   turtle.fd(50)
-  shape(4)
+  shape(5)
 turtle.goto(-100,100)
 for n in range(9):
   turtle.fd(50)
